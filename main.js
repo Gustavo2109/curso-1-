dec52e7bf25f5942f04c6083509726a582c1573b
Gustavo2109 <118767724+Gustavo2109@users.noreply.github.com>
2024-03-13 11:14:37 -0300

    15

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 const botoes = document.querySelectorAll(".botao");
-console.log(botões);
+
+for (let i = 0; i < botoes.length; i++) {
+    botoes[i].onclick = function () {
+
+        for (let j = 0; j < botoes.length; j++) {
+            botoes[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+    }
+}
+
 
 
