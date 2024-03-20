f983b32afa2e36dd338349b6334f1e0b5129861a
Gustavo2109 <118767724+Gustavo2109@users.noreply.github.com>
2024-03-20 10:59:16 -0300

    20

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-const botoes = document.querySelectorAll(".botao");
+const botoes = document.querySelectorAll(".botao");//variavel que armazena todos os itens com a classe botão
 
 
-for (let i = 0; i < botoes.length; i++) {
-    botoes[i].onclick = function () {
+for (let i = 0; i < botoes.length; i++) {//loop que executa enquanto i<quantidade de botoes
+    botoes[i].onclick = function () {//cria uma função
 
-        for (let j = 0; j < botoes.length; j++) {
-            botoes[j].classList.remove("ativo");
+        for (let j = 0; j < botoes.length; j++) { //logo que executa j<quantidade de botoes
+            botoes[j].classList.remove("ativo");//remove a palavra ativo da classe.
         }
-        botoes[i].classList.add("ativo");
+        botoes[i].classList.add("ativo");//adiciona a palavra ativo da classe.
     }
 }
+
 
 
 
