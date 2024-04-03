7a443c76ea2552c0504089647b4b03600d8e25c6
Gustavo2109 <118767724+Gustavo2109@users.noreply.github.com>
2024-04-03 11:44:28 -0300

    03/04/2024

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const botoes = document.querySelectorAll(".botao");//variavel que armazena todos os itens com a classe botão
-onst textos = document.querySelectorAll(".aba-conteudo");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for (let i = 0; i < botoes.length; i++) {//loop que executa enquanto i<quantidade de botoes
     botoes[i].onclick = function () {//cria uma função
@@ -15,7 +15,41 @@ for (let i = 0; i < botoes.length; i++) {//loop que executa enquanto i<quantidad
 }
 
 const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+const tempoObjetivo2 = new Date("2024-12-05T00:00:00")
+const tempoObjetivo3 = new Date("2024-10-30T00:00:00")
+const tempoObjetivo4 = new Date("2024-10-01T00:00:00")
 
-contadores[0].textContent = "Contagem regressiva";
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
+function calculaTempo(tempoObjetivo) {
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+    let segundos = Math.floor(tempoFinal / 1000);
+    let minutos = Math.floor(segundos / 60);
+    let horas = Math.floor(minutos / 60);
+    let dias = Math.floor(horas / 24);
 
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+    if (tempoFinal > 0) {
+
+        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+    } else {
+        return "prazo finalizado"
+    }
+}
+
+function atualizaCronometro() {
+    for (let i = 0; i < contadores.length; i++) {
+        contadores[0].textContent = calculaTempo(tempos[1]);
+    }
+}
+
+function comecacronometro() {
+    atualizaCronometro ();
+    setInterval(atualizaCronometro, 1000);
+}
+
+comecacronometro();
