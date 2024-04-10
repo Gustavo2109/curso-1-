ef1b81b36f1b65741411335caf8e3f51accf15c0
Gustavo2109 <118767724+Gustavo2109@users.noreply.github.com>
2024-04-10 11:40:25 -0300

    oi

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {//loop que executa enquanto i<quantidad
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2024-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2025-02-01T00:00:00");
