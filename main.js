d6f5b2901c03d81498bd448172afa51dfa25a25d
Gustavo2109 <118767724+Gustavo2109@users.noreply.github.com>
2024-04-10 11:12:55 -0300

    10/04/2024

--- main.js
+++ main.js
@@ -34,22 +34,27 @@ function calculaTempo(tempoObjetivo) {
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0) {
-
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
+        return [dias,horas,minutos,segundos];
     } else {
-        return "prazo finalizado"
+        return [0,0,0,0]
     }
+
 }
 
-function atualizaCronometro() {
-    for (let i = 0; i < contadores.length; i++) {
-        contadores[0].textContent = calculaTempo(tempos[1]);
+
+function atualizaCronometro(){
+for (let i=0; i<contadores.length;i++){
+    document.getElementById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
-}
+}  
 
 function comecacronometro() {
-    atualizaCronometro ();
-    setInterval(atualizaCronometro, 1000);
+atualizaCronometro();
+setInterval(atualizacronometro, 1000);
+
 }
 
 comecacronometro();
