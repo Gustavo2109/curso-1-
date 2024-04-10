c1c9bb2e94638ba3f88304bf3690c704dd43487c
Gustavo2109 <118767724+Gustavo2109@users.noreply.github.com>
2024-04-10 11:43:26 -0300

    siuuu

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0) {
         return [dias,horas,minutos,segundos];
     } else {
-        return [0,0,0,0]
+        return [0,0,0,0];
     }
 
 }
